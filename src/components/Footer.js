c0a513ecc0c3ad2ee2c02df1dffb40ff7f951065
Mary C <mcn4c@virginia.edu>
2021-04-11 14:43:46 -0400

    created src folder with components folder and added AboutPage component and Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+function Footer() {
+
+	return (
+
+		
+		
+		<footer style={{position: "fixed"}}>
+		  <p>@ Mary Nash November 2020</p>
+		</footer>
+
+	)
+
+}
+
+export default Footer;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+//may  not need if don't use bootstrap for styling...tbd
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
